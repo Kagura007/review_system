09b01695bb6d247291e3d0385b1651288a3c057f
Kagura007 <kagura.programmazione@gmail.com>
2026-07-13 00:25:29 +0900

    Implement: form.blade.php

--- resources/views/reviews/form.blade.php
+++ resources/views/reviews/form.blade.php
@@ -12,8 +12,23 @@
 <body>
     <main class="p-review-form">
 
-        <section>
-            <h1>ようこそ、フォーム画面へ。</h1>
+        <section class="p-review-form__title-group">
+            <h1 class="p-review-form__title">ようこそ、口コミ投稿フォームへ。</h1>
+        </section>
+
+        <section class="p-review-form__form-content">
+
+            <form action="" method="POST" class="p-review-form__form">
+                @csrf
+                <div class="p-review-form__item-group">
+                    <label for="content" class="p-review-form__label">投稿内容</label>
+                    <textarea name="content" id="content" cols="50" rows="10" required class="p-review-form__textarea">
+                    </textarea>
+                </div>
+
+                <button type="submit" class="p-review-form__button">投稿する</button>
+            </form>
+
         </section>
 
     </main>
